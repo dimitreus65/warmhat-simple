9d30b294624795e068b6b019db763f82481fce66
dimitreus65 <dimitreus65@gmail.com>
2025-11-13 16:38:10 +0100

    add animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,22 +78,6 @@ export default {
         sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
-        'accordion-down': {
-          from: {
-            height: '0',
-          },
-          to: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-        },
-        'accordion-up': {
-          from: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-          to: {
-            height: '0',
-          },
-        },
         'fade-in': {
           '0%': {
             opacity: '0',
@@ -107,11 +91,11 @@ export default {
         'slide-in': {
           '0%': {
             opacity: '0',
-            transform: 'translateX(-20px)',
+            transform: "scale(0.95)",
           },
           '100%': {
             opacity: '1',
-            transform: 'translateX(0)',
+            transform: "scale(1)",
           },
         },
         'bounce-light': {
@@ -124,9 +108,7 @@ export default {
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in': 'fade-in 0.5s ease-out forwards',
+        'fade-in': 'fade-in 0.5s ease-out',
         'slide-in': 'slide-in 0.5s ease-out forwards',
         'bounce-light': 'bounce-light 3s ease-in-out infinite',
       },
